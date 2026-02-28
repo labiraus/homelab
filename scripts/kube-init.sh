f62e4714bcc0c5cbced28fcaea624a3a77b43036
Oliver Hathaway <oliver@labiraus.com>
2026-02-28 23:24:00 +0000

    feat: add OCI secret creation for GitHub Container Registry in kube-init script

--- scripts/kube-init.sh
+++ scripts/kube-init.sh
@@ -3,9 +3,18 @@ set -euo pipefail
 
 flux install --namespace=flux-system
 
+flux create secret oci ghcr-creds \
+  --namespace=flux-system \
+  --url=ghcr.io \
+  --username=$GITHUB_USERNAME \
+  --password=$GITHUB_TOKEN
+
+  # If recreating source:
 flux create source oci flux-bootstrap \
+  --namespace=flux-system \
   --url=oci://ghcr.io/labiraus/homelab/helm/flux-bootstrap \
   --tag=uat \
+  --secret-ref=ghcr-creds \
   --interval=10m \
   --export | kubectl apply -f -
 
@@ -16,3 +25,4 @@ flux create helmrelease flux-bootstrap \
   --wait=true \
   --interval=10m \
   --export | kubectl apply -f -
+
